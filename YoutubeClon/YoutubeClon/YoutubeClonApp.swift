4e20a57a42d390e49eea116f9b2514bf71909e09
Adrian Javier Hernandez Gutierrez <adrianjavierhernandezgutierrez@MacBook-Pro-de-INFRAESTRUCTURA.local>
2024-03-15 12:43:19 -0600

    Initial Commit

---
+++ YoutubeClon/YoutubeClon/YoutubeClonApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeClonApp.swift
+//  YoutubeClon
+//
+//  Created by Adrian Javier Hernandez Gutierrez on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeClonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
